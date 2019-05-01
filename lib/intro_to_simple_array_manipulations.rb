44df2516b35e0d056a701b35aa435e0aa4830c82
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 01:25:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,16 +10,16 @@ def using_pop (array)
   array.pop
 end
 
-def pop_with_args (array, n)
-  array.pop(n)
+def pop_with_args (array, num)
+  array.pop(num)
 end
 
 def using_shift (array)
   array.shift
 end
 
-def shift_with_args (array, n)
-  array.shift(n)
+def shift_with_args (array, num)
+  array.shift(num)
 end
 
 def using_concat (arr1, arr2)
