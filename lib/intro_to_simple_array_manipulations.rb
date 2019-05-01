867c94c55aadb25dccb033a56415a324c2a248ba
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 01:17:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push (arg1, arg2)
+
+end
+
+def using_unshift (arg1, arg2)
+
+end
+
+def using_pop (array)
+
+end
+
+def pop_with_args (array, n)
+
+end
+
+def using_shift (array)
+
+end
+
+def shift_with_args (array, n)
+
+end
+
+def using_concat (arr1, arr2)
+
+end
+
+def using_insert (arr, element)
+
+end
+
+def using_uniq (array)
+
+end
+
+def using_flatten (array)
+
+end
+
+def using_delete (array, string)
+
+end
+
+def using_delete_at (array, n)
+
+end
